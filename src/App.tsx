ef622cd5c46776c03833add47e78ebea07a1ecaf
mazurenka <an.drej@mail.ru>
2022-03-02 12:07:04 +0100

    Localstorage, set, get, clear, remove

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [value, setValue] = useState(0)
+    const increment = () => {
+        setValue(value + 1)
+    }
+
+    const setLocal = () => {
+        localStorage.setItem('valueKey', JSON.stringify(value))
+        localStorage.setItem('valueKey + 1', JSON.stringify(value + 1))
+    }
+    const getLocal = () => {
+        let newValue = localStorage.getItem('valueKey')
+        if (newValue) {
+            let setValueNew = JSON.parse(newValue)
+          setValue(setValueNew)
+        }
+    }
+    const clearLocal = () => {
+        localStorage.clear()
+        setValue(0)
+    }
+    const clearItemLocal = () => {
+        localStorage.removeItem('valueKey + 1')
+    }
+
+    return (
+        <div className="App">
+            <h1> {value} </h1>
+            <button onClick={increment}>inc</button>
+            <button onClick={setLocal}>setLocalstorage</button>
+            <button onClick={getLocal}>getLocalstorage</button>
+            <button onClick={clearLocal}>clearLocalstorage</button>
+            <button onClick={clearItemLocal}>clearItemLocalstorage</button>
+
+        </div>
+    );
 }
 
 export default App;
