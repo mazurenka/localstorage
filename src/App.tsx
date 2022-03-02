c30e7cabcdaae7c3e19a81f84a56ada9e4415531
mazurenka <an.drej@mail.ru>
2022-03-02 13:04:13 +0100

    localstorage useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+    console.log('App')
     const [value, setValue] = useState(0)
+    console.log('value: ', value)
     const increment = () => {
         setValue(value + 1)
     }
 
     useEffect(() => {
+        console.log('useEffect with getItem')
         let save = localStorage.getItem('valueKey')
         if (save) {
             let saveValue = JSON.parse(save)
@@ -16,15 +19,17 @@ function App() {
     }, [])
 
     useEffect(() => {
+        console.log('useEffect with setItem')
         localStorage.setItem('valueKey', JSON.stringify(value))
 
     }, [value])
+
 
     const resetIncrement = () => {
         localStorage.clear()
         setValue(0)
     }
-
+    console.log('render JSX')
     return (
         <div className="App">
             <h1> {value} </h1>
